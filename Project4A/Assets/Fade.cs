b6571fcdefb756379faab52a79ec8eee02338087
Eric Walsh <walshericc@gmail.com>
2019-04-09 15:59:37 -0400

    changes made during class

---
+++ Project4A/Assets/Fade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var faded = animator.GetBool("Faded");
+            animator.SetBool("Faded", !faded);
+        }
+    }
+
+    public void PrintMessage()
+    {
+        Debug.Log("Fade half done");
+    }
+}
